766092557069f4c2e3e3228f6ce34c98b69c1b87
KoofNG <isrealakpan296@gmail.com>
2019-03-21 02:18:59 +0100

    Created Navigation and footer components

---
+++ src/components/Footer.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="footer">
+        <p>&copy; 2019 - Book Information Retrieval System</p>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+div#footer{
+    height: 50px;
+    width: 100%;
+    padding: 10px 20px;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    background-color: #fafafa;
+}
+div#footer>p{
+    padding: 7.5px 0;
+    margin: 0;
+    font-size: 14px;
+    font-weight: bold;
+    color: #121212;
+}
+</style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="navigation" class="pad-top-bottom">
+    <div class="brandName"><h1>Book Information Retrieval System</h1></div>
+    <div class="accounts"><a href="">Login</a></div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+/* Block */
+div#navigation{
+  display: flex;
+  justify-content: space-between;
+  height: 60px;
+  box-shadow: 0 1px 1px rgba(0, 0, 0, 0.1);
+  background: #fff;
+  padding-left: 50px;
+  padding-right: 50px; 
+}
+div.brandName{
+  height: 100%;
+  width: auto;
+}
+div.accounts{
+  height: 44px;
+  width: auto;
+  padding: 7px 0px;
+  position: relative;
+  display: block;
+}
+/* Elements */
+div.brandName>h1{
+  margin: 0;
+  padding: 9px 0px;
+  font-size: 22px;
+  font-weight: 400;
+  color: green;
+}
+div.accounts>a{
+  display: block;
+  position: relative;
+  padding: 7px 20px;
+  text-decoration: none;
+  text-transform: uppercase;
+  color: #ffffff;
+  font-size: 14px;
+  font-weight: 500;
+  border-radius: 20px;
+  background-color: green;
+
+}
+/* Modifiers */
+div.pad-top-bottom{
+  padding-top: 0.5rem;
+  padding-bottom: 0.5rem;
+  padding-left: 1rem;
+  padding-right: 1rem;
+}
+</style>
