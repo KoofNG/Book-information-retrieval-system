e96bbe1b900b23dff23a2f1a79b0e128ecc920b6
KoofNG <isrealakpan296@gmail.com>
2019-04-08 19:39:50 +0100

    Desingned the loader component

---
+++ src/components/Loader.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <div id="loader"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+div#loader {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    z-index: 1;
+    width: 75px;
+    height: 75px;
+    margin: -37.5px 0 0 -37.5px;
+    border: 5px solid #f3f3f3;
+    border-radius: 50%;
+    border-top: 5px solid #008000;
+    -webkit-animation: spin 2s linear infinite;
+    animation: spin 2s linear infinite;
+    z-index: 1000;
+    display: none;
+}
+div#loader.active{
+    display: block;
+}
+@-webkit-keyframes spin {
+    0% { -webkit-transform: rotate(0deg); }
+    100% { -webkit-transform: rotate(360deg); }
+}
+@keyframes spin {
+    0% { transform: rotate(0deg); }
+    100% { transform: rotate(360deg); }
+}
+</style>
