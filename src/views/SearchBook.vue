bcd0d54cfe0891b9609a30a83a198d97918a10e2
KoofNG <isrealakpan296@gmail.com>
2019-04-12 14:34:52 +0100

    Conditioned the layout for results and no results

--- src/views/SearchBook.vue
+++ src/views/SearchBook.vue
@@ -5,20 +5,19 @@
       <Search @search="find"/>
       <div id="search-result">
         <Loader/>
-        <h3>Search Results</h3>
-        <div id="book-search-result-card" v-for="(item, index) in searchResult" :key="index">
-          <h4>{{item.title}}</h4>
-          <h5>{{item.author}}</h5>
-          <h6>{{new Date(item.publicationYear).getFullYear()}}</h6>
-          <h6>{{item.edition}}</h6>
-          <p>
-            Quantity Available:
-            <span id="availability">{{item.quantity}}</span>
-          </p>
-
-          <div>
-            <button id="edit" @click="editBook(item._id)">edit</button>
-            <button id="delete" @click="bookDelete(item._id)">delete</button>
+        <div class="result">
+          <h3 id="header">Search Results</h3>
+          <div id="book-search-result-card" v-for="(item, index) in searchResult" :key="index">
+            <h4>{{item.title}}</h4>
+            <h5>{{item.author}}</h5>
+            <h6>{{new Date(item.publicationYear).getFullYear()}}</h6>
+            <h6>{{item.edition}}</h6>
+            <p>Quantity Available:<span id="availability">{{item.quantity}}</span></p>
+            <div>
+              <button id="request">request this book</button>
+              <button id="edit" @click="editBook(item._id)">edit</button>
+              <button id="delete" @click="bookDelete(item._id)">delete</button>
+            </div>
           </div>
         </div>
       </div>
@@ -28,21 +27,23 @@
 </template>
 
 <script>
-import Navigation from "@/components/Navigation.vue";
-import Search from "@/components/Search.vue";
-import Footer from "@/components/Footer.vue";
-import Loader from "@/components/Loader.vue";
+import Navigation from '@/components/Navigation.vue';
+import Search from '@/components/Search.vue';
+import Footer from '@/components/Footer.vue';
+import Loader from '@/components/Loader.vue';
+
 export default {
   components: {
     Navigation,
     Search,
     Loader,
-    Footer
+    Footer,
   },
 
-  data: function() {
+  data() {
     return {
-      searchResult: []
+      isActive: false,
+      searchResult: [],
     };
   },
 
@@ -51,37 +52,37 @@ export default {
       this.searchResult = result.slice();
     },
 
-    editBook: function(id) {
+    editBook(id) {
       this.$router.push({ path: `/updateBook/${id}` });
     },
-
     bookDelete(id) {
-      fetch("http://192.168.43.220:8080/books/remove/" + id)
+      fetch(`http://localhost:8081/books/remove/${id}`)
         .then(res => res.json())
-        .then(res => {
-          if (res.status == "unsuccesful") {
-            alert("Book not deleted");
+        .then((res) => {
+          if (res.status == 'unsuccesful') {
+            alert('Book not deleted');
           } else {
-            alert("Book deleted");
-            this.searchResult = this.searchResult.filter(result => {
-              return result._id != id;
-            });
+            alert('Book deleted');
+            this.searchResult = this.searchResult.filter(result => result._id != id);
           }
+        })
+        .catch((err)=>{
+          alert('Book not Deleted');
         });
       // console.log(id)
-    }
-  }
+    },
+  },
 };
 </script>
 
 <style>
 span#availability {
-  padding: 2px 10px !important;
-  margin: 0 4px;
+  padding: 2px 7.5px !important;
+  margin: 0 7.5px;
   background-color: #008000;
   color: #ffffff;
   position: relative;
-  border-radius: 5px;
+  border-radius: 50%;
 }
 div#search-w {
   display: flex;
@@ -90,15 +91,22 @@ div#search-w {
   width: 100%;
   height: 100%;
   padding: 30px 0px;
+  background: url('../assets/bgp.jpg');
+  background-position: 50% 50%;
+  background-size: cover;
+  background-repeat: no-repeat;
 }
-div#search-result {
-  width: 65%;
+div#search-result div.result {
+  width: 100%;
   padding: 0 30px;
   position: relative;
-  display: block;
+  display: none;
   text-align: left;
 }
-div#search-result > h3 {
+div#search-result div.result.active {
+  display: block;
+}
+div#search-result div.result > h3 {
   font-weight: lighter;
   font-size: 1.3em;
   color: green;
@@ -113,7 +121,8 @@ div#book-search-result-card {
   display: block;
   position: relative;
   box-shadow: 2px 0px 5px 1px #cccccc;
-}
+  background-color: #ffffff;
+  opacity: 1;}
 div#book-search-result-card > div {
   width: 100%;
   height: 40px;
@@ -137,6 +146,10 @@ div#book-search-result-card > div > button#delete {
 }
 div#book-search-result-card > div > button#edit {
   background-color: blue;
+  color: #ffffff;
+}
+div#book-search-result-card > div > button#request {
+  background-color: #008000;
   color: #ffffff;
 }
 div#book-search-result-card * {
