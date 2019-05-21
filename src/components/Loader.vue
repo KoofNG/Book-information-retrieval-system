0fe5adf7503adc5696fc743150e33dc402f7eff0
KoofNG <isrealakpan296@gmail.com>
2019-05-21 12:08:35 +0100

    Reduced the loader height and width

--- src/components/Loader.vue
+++ src/components/Loader.vue
@@ -14,8 +14,8 @@ div#loader {
     left: 50%;
     top: 50%;
     z-index: 1;
-    width: 75px;
-    height: 75px;
+    width: 60px;
+    height: 60px;
     margin: -37.5px 0 0 -37.5px;
     border: 5px solid #f3f3f3;
     border-radius: 50%;
@@ -23,11 +23,11 @@ div#loader {
     -webkit-animation: spin 2s linear infinite;
     animation: spin 2s linear infinite;
     z-index: 1000;
-    display: none;
-}
-div#loader.active{
     display: block;
 }
+/* div#loader.active{
+    display: block;
+} */
 @-webkit-keyframes spin {
     0% { -webkit-transform: rotate(0deg); }
     100% { -webkit-transform: rotate(360deg); }
